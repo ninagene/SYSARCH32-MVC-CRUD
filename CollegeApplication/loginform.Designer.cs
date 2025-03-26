0a97da72ff4755c8879ce0060ec61bc191d1152a
MarteDan <aresdmarte@gmail.com>
2025-03-26 07:18:54 -0700

    Updated .gitignore to exclude VS files

---
+++ CollegeApplication/loginform.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace CollegeApplication
+{
+    partial class loginform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernamebtn = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.passwordbtn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernamebtn
+            // 
+            this.usernamebtn.Location = new System.Drawing.Point(186, 90);
+            this.usernamebtn.Multiline = true;
+            this.usernamebtn.Name = "usernamebtn";
+            this.usernamebtn.Size = new System.Drawing.Size(273, 24);
+            this.usernamebtn.TabIndex = 0;
+            this.usernamebtn.TextChanged += new System.EventHandler(this.usernamebtn_TextChanged);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(23, 94);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(109, 20);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "USERNAME";
+            // 
+            // passwordbtn
+            // 
+            this.passwordbtn.Location = new System.Drawing.Point(186, 129);
+            this.passwordbtn.Multiline = true;
+            this.passwordbtn.Name = "passwordbtn";
+            this.passwordbtn.Size = new System.Drawing.Size(273, 24);
+            this.passwordbtn.TabIndex = 0;
+            this.passwordbtn.TextChanged += new System.EventHandler(this.passwordbtn_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 133);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PASSWORD";
+            // 
+            // loginbtn
+            // 
+            this.loginbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginbtn.Location = new System.Drawing.Point(135, 195);
+            this.loginbtn.Margin = new System.Windows.Forms.Padding(2);
+            this.loginbtn.Name = "loginbtn";
+            this.loginbtn.Size = new System.Drawing.Size(204, 46);
+            this.loginbtn.TabIndex = 5;
+            this.loginbtn.Text = "LOGIN";
+            this.loginbtn.UseVisualStyleBackColor = true;
+            this.loginbtn.Click += new System.EventHandler(this.loginbtn_Click);
+            // 
+            // loginform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 450);
+            this.Controls.Add(this.loginbtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.passwordbtn);
+            this.Controls.Add(this.usernamebtn);
+            this.Name = "loginform";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernamebtn;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox passwordbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button loginbtn;
+    }
+}
---
+++ CollegeApplication/loginform.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CollegeApplication
+{
+    public partial class loginform : Form
+    {
+        private string connectionString = "server=localhost;database=college_db;uid=root;pwd=;";
+
+        public loginform()
+        {
+            InitializeComponent();
+
+            // Set tab order
+            usernamebtn.TabIndex = 0;
+            passwordbtn.TabIndex = 1;
+            loginbtn.TabIndex = 2;
+
+            // Censor password input
+            passwordbtn.PasswordChar = '*';
+        }
+
+        private void usernamebtn_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void passwordbtn_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginbtn_Click(object sender, EventArgs e)
+        {
+            string username = usernamebtn.Text.Trim();
+            string password = passwordbtn.Text.Trim();
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter both username and password.", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = "SELECT COUNT(*) FROM users WHERE username = @username AND password = @password";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@username", username);
+                        cmd.Parameters.AddWithValue("@password", password);
+
+                        int userExists = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        if (userExists > 0)
+                        {
+                            MessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            // Proceed to next form or main dashboard
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid username or password.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Database connection error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
